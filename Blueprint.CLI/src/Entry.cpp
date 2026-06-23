f1f24a4aa788842e85f5e6ec8512657e0ecea9da
D1pper <malecszymon76@gmail.com>
2026-06-23 15:14:40 +0200

    reset

--- Blueprint.CLI/src/Entry.cpp
+++ Blueprint.CLI/src/Entry.cpp
@@ -2,15 +2,8 @@
 #include <iostream>
 
 #include "Terminal.hpp"
-#include "Services.hpp"
-
-//
-// Here will lie as follows:
-// The Terminal Interface
-//
-
 
 int main()
 {
-	std::print("{}", Services::GetBlueprintsString("Exp 1.2"));
+	
 } 
