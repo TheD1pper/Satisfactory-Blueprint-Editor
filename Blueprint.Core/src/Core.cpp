59c194cfc5dc8d4d0afaedfc7b8b81f42a919797
D1pper <malecszymon76@gmail.com>
2026-07-04 11:37:11 +0200

    e

--- Blueprint.Core/src/Core.cpp
+++ Blueprint.Core/src/Core.cpp
@@ -12,12 +12,10 @@ namespace Core
 		std::print("X: {}, Y: {}, Z: {}\n", Size.X, Size.Y, Size.Z);
 
 		std::print("======Cost Entries======\n");
-
 		for (auto& Entry : CostEntries)
 			std::print("Class name: {} \n",Entry.ClassName.Content);
 
 		std::print("======Content Entries======\n");
-
 		for (auto& Entry : ContentEntries)
 			std::print("Class Name: {}\n", Entry.ClassName.Content);
 	}
