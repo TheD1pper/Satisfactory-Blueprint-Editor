f7724a4f6c172e0c9a083bb556f1ef87bc838113
D1pper <malecszymon76@gmail.com>
2026-07-09 08:48:28 +0200

    Moved method implementations of Parser::InputBlueprint class regarding sequencial binary readings

--- Blueprint.Parser/include/Parser.hpp
+++ Blueprint.Parser/include/Parser.hpp
@@ -10,6 +10,8 @@ namespace fs = std::filesystem;
 
 namespace Parser
 {
+	int constexpr MaxChunkSize = 0x00020000;
+
 	class InputBlueprint
 	{
 	private:
@@ -18,17 +20,20 @@ namespace Parser
 
 	public:
 		Result<Core::BlueprintHeader> ReadHeader();
+		Result<Core::BlueprintBody> ReadBody();
 		uint64_t GetBytesRead() const;
+
 
 	private:
 		template <typename T> Result<T> Read();
 		void SkipBytes(std::streamsize _Bytes);
 		std::istream& ReadBytes(char* _String, uint64_t _Count);
 
+	private:
 
 	public:
 		InputBlueprint() = default;
-		InputBlueprint(fs::path& _Path);
+		InputBlueprint(fs::path _Path);
 
 	};
 }
