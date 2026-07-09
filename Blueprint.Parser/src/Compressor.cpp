2e68e7861aea5905eea14e659718837b7fd1d759
D1pper <malecszymon76@gmail.com>
2026-07-09 20:10:27 +0200

    Fixed C4267 warning (possible loss of data)

--- Blueprint.Parser/src/Compressor.cpp
+++ Blueprint.Parser/src/Compressor.cpp
@@ -7,10 +7,10 @@ namespace Parser
 	Zlib Z;
 	Result<ByteVector> Zlib::Compress(const ByteVector& _Input)
 	{
-		uLongf CompressedSize = compressBound(_Input.size());
+		uLongf CompressedSize = compressBound(static_cast<uLong>(_Input.size()));
 		ByteVector Output(CompressedSize);
 
-		if (compress(Output.data(), &CompressedSize, _Input.data(), _Input.size()) != Z_OK)
+		if (compress(Output.data(), &CompressedSize, _Input.data(), static_cast<uLong>(_Input.size())) != Z_OK)
 			return std::unexpected(Eh::Error(Eh::Compression::Fail, "Compression failed"));
 
 		Output.resize(CompressedSize);
@@ -20,9 +20,9 @@ namespace Parser
 	Result<ByteVector> Zlib::Decompress(const ByteVector& _Input, const size_t& DecompressedSize)
 	{
 		ByteVector Output(DecompressedSize);
-		uLongf Size = DecompressedSize;
+		uLongf Size = static_cast<uLongf>(DecompressedSize);
 
-		if (uncompress(Output.data(), &Size, _Input.data(), _Input.size()) != Z_OK)
+		if (uncompress(Output.data(), &Size, _Input.data(), static_cast<uLong>(_Input.size())) != Z_OK)
 			return std::unexpected(Eh::Error(Eh::Compression::Fail, "Decompression failed"));
 
 		Output.resize(Size);
