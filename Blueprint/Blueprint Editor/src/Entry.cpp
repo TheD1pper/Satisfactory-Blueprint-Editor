4caf40f5b04a389583b3cdf76dfd6377e8c4a6ce
D1pper <malecszymon76@gmail.com>
2026-08-22 11:31:09 +0200

    Add implementation in Entry.cpp

--- Blueprint/Blueprint Editor/src/Entry.cpp
+++ Blueprint/Blueprint Editor/src/Entry.cpp
@@ -1,5 +1,12 @@
+#include <Windows.h>
+#include <stdio.h>
+
+import std;
+
+import Editor.Application;
 
 int main() 
 {
-
+	Editor::Application SBE("Satisfactory Blueprint Editor", 800, 600);
+	SBE.Run();
 }
